d0c758e9dd1f26814e2719b5a830c38e9f6fb76a
akseart <akseart@ya.ru>
2021-12-03 20:09:47 +0300

    Refactor and create model input

--- app.py
+++ app.py
@@ -14,15 +14,13 @@ def create_app():
         page = request.args.get(get_page_parameter(), type=int, default=1)
 
         per_page = 15
-        books = configure_and_run.book.get_list_of_book(page, per_page)
+        books = configure_and_run.book.get_part(page, per_page)
         pagination = Pagination(page=page,
                                 total=len(configure_and_run.book),
                                 search=False,
                                 record_name='книги',
                                 per_page=per_page,
-                                format_total=False,
-                                format_number=False,
-                                display_msg="Отображены <b>{start} - {end}</b> {record_name} из <b>{total}</b>"
+                                display_msg=" "
                                 )
 
         return render_template('index.html',
@@ -36,8 +34,8 @@ def create_app():
             book = configure_and_run.book[int(book_id)]
         except IndexError:
             return "Not found", 404
-        recomend = [configure_and_run.book[x] for x in book["recomend"]]
-        return render_template('books.html', books=book, recomend=recomend)
+        recommend = [configure_and_run.book[x] for x in configure_and_run.book.get_recommend(book_id)]
+        return render_template('books.html', books=book, recommend=recommend)
 
     return app
 
